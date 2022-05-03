ade6608c898576d8397011b21c7afe5d9df60c50
Ayush Iyankan <75990868+AyushIyankan@users.noreply.github.com>
2022-05-03 13:38:51 +0530

    fix calc

--- src/components/PunkList.js
+++ src/components/PunkList.js
@@ -4,15 +4,16 @@ import "../css/PunkList.css";
 import HorizontalScroll from "react-horizontal-scrolling";
 
 const PunkList = ({ punkListData, setSelectedPunk }) => {
-  const handleClick = (e) => {
-    console.log(e.id);
-  };
+  console.log(punkListData);
   return (
     <HorizontalScroll>
       <div className="punkList">
         {punkListData.map((punk) => {
           return (
-            <div key={punk.id} onClick={() => setSelectedPunk([punk.token_id])}>
+            <div
+              key={punk.id}
+              onClick={() => setSelectedPunk([12 - punk.token_id])}
+            >
               <CollectionCard key={punk.token_id} {...punk} />
             </div>
           );
